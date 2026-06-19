2cdc7cf5074ea3cb5abfa58a19195faba9429aa5
Schnellerman <33846117+Schnellerman@users.noreply.github.com>
2026-06-19 19:38:31 +0300

    Update vulnerable.sol

--- Solidity/01-precision-loss/vulnerable.sol
+++ Solidity/01-precision-loss/vulnerable.sol
@@ -1,1 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+contract Vulnerable {
+  uint256 public constant REWARD_RATE = 500;
+  uint256 public constant BASIS_POINTS = 10000;
+
+  function calculateReward(uint256 amount, uint256 daysActive) public pure returns (uint256) {
+    uint256 rewardShare = amount / BASIS_POINTS;
+
+    return rewardShare * REWARD_RATE * daysActive;
+  }
+}
