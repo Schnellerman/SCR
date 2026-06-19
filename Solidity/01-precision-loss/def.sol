f42bfd0652c43ee001a6f4ba3dbb29f395a50359
Schnellerman <33846117+Schnellerman@users.noreply.github.com>
2026-06-19 19:44:03 +0300

    Update def.sol

--- Solidity/01-precision-loss/def.sol
+++ Solidity/01-precision-loss/def.sol
@@ -1,1 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+contract FixedReward {
+  uint256 public constant REWARD_RATE = 500;
+  uint256 public constant BASIS_POINTS = 10000;
+
+  function calculateRewardSecure(uint256 amount, uint256 daysActive)
+}
