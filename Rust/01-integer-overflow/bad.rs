053acdb96cf29e2dc1503a9b3a10d489aadf84eb
Schnellerman <33846117+Schnellerman@users.noreply.github.com>
2026-06-19 18:51:59 +0300

    Update bad.rs

--- Rust/01-integer-overflow/bad.rs
+++ Rust/01-integer-overflow/bad.rs
@@ -1,1 +1,8 @@
+// bad.rs
+pub fn process_withdrawal(balance: u64, amount: u64, fee: u64) -> Result<u64, &'static str> {
+    if balance < amount + fee {
+        return Err("Insufficient funds");
+    }
 
+    Ok(balance - (amount + fee))
+}
